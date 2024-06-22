16ba188d2ba9a7d13070170617be4671fe827118
phamhaicnttk21 <phamhaicnttk21@gmail.com>
2024-06-22 23:55:12 +0700

    user

---
+++ src/main/java/learn/microservice/userService/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package learn.microservice.userService.Repository;
+
+import learn.microservice.userService.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUserId(Long userId);
+}
